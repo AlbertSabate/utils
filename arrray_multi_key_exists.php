998503d7678fb7800d88b68f8c803f08a982a47a
Albert Sabaté <albertsabate1@gmail.com>
2013-01-28 18:27:02 +0100

    Se modifica la documentación de array_multi_key_exists

--- arrray_multi_key_exists.php
+++ arrray_multi_key_exists.php
@@ -3,16 +3,17 @@
 /**
  * Función que utiliza array_key_exists con un array de keys en vez de un único key.
  * 
- * @Author: Albert Sabaté
- * @date: 25 / 01 / 2013
+ * @Author  Albert Sabaté
+ * @date    25/01/2013
  * 
- * @param array $arrKeys
- * @param array $array
+ * @param   array       $arrKeys
+ * @param   array       $array
+ * @param   boolean     $match      dejar esta variable sin settear.
  * 
- * @return boolean true si encuentra todos los keys (case sensitive)
+ * @return  boolean     true si encuentra todos los keys (case sensitive)
  * 
  */
-function array_multi_key_exists(array $arrKeys, array $array, /* variable usada para recursión */$match = true) {
+function array_multi_key_exists(array $arrKeys, array $array, $match = true) {
     $matched = array_key_exists(array_shift($arrKeys), $array);
 
     if ($match === false) return false;
